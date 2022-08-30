093f2d4d10ca0768c44a63ea028297eeaf593ce6
Tetiana <dyopka@gmail.com>
2022-08-30 00:59:22 -0500

    created byejava

---
+++ src/First/ByeWorld.java
@@ -0,0 +1,9 @@
+package First;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("5 weeks of java left");
+        System.out.println("1.5 weeks of git");
+
+    }
+}
